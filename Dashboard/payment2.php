a952c2fdef8e458517c2be4708e0ad8a047d01b8
MANI <maniadishu7342@gmail.com>
2022-07-24 13:34:23 +0530

    removed payment in student

--- Dashboard/payment2.php
+++ Dashboard/payment2.php
@@ -203,32 +203,7 @@
                 }
                  else{?>
               <ul>
-                <li class="relative px-6 py-3">
-                    <span
-                    class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                    aria-hidden="true"
-                  ></span>
-                  <a
-                    class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                    href="payment2.php"
-                  >
-                    <svg
-                      class="w-5 h-5"
-                      aria-hidden="true"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                      ></path>
-                    </svg>
-                    <span class="ml-4">Payments</span>
-                  </a>
-                </li>
+                
                 <li class="relative px-6 py-3">
                     <a
                       class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
